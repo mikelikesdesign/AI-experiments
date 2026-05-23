3640ab2f87c60be6558d60f11f181609149b1670
Michael <mike@ricecakesmedia.com>
2026-05-23 20:43:11 +0900

    Add pinch text experiment

---
+++ pinch-text/pinch-textTests/pinch_textTests.swift
@@ -0,0 +1,17 @@
+//
+//  pinch_textTests.swift
+//  pinch-textTests
+//
+//  Created by Michael Lee on 5/23/26.
+//
+
+import Testing
+@testable import pinch_text
+
+struct pinch_textTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ pinch-text/pinch-textUITests/pinch_textUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  pinch_textUITestsLaunchTests.swift
+//  pinch-textUITests
+//
+//  Created by Michael Lee on 5/23/26.
+//
+
+import XCTest
+
+final class pinch_textUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
