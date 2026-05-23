86144e02bc286a2f861c26433ee2bcd64608c1f5
Michael <mike@ricecakesmedia.com>
2026-05-23 20:37:27 +0900

    Add reading tracker experiment

---
+++ reading tracker/reading tracker/reading_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  reading_trackerApp.swift
+//  reading tracker
+//
+//  Created by Michael Lee on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
