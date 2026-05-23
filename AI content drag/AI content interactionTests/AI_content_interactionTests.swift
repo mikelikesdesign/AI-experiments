4d8b9a4aa0ada42f7bd5183415c48c531b2cd5ca
Michael <mike@ricecakesmedia.com>
2026-05-23 21:20:56 +0900

    Add AI content drag experiment

---
+++ AI content drag/AI content interactionTests/AI_content_interactionTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_content_interactionTests.swift
+//  AI content interactionTests
+//
+//  Created by Michael Lee on 11/11/24.
+//
+
+import Testing
+@testable import AI_content_interaction
+
+struct AI_content_interactionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
