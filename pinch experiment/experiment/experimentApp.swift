617a0901605728839090e99e633eac942b46f1ce
Michael <mike@ricecakesmedia.com>
2026-05-23 21:24:25 +0900

    Add pinch experiment

---
+++ pinch experiment/experiment/experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  experimentApp.swift
+//  experiment
+//
+//  Created by Michael Lee on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pinch experiment/experimentTests/experimentTests.swift
@@ -0,0 +1,17 @@
+//
+//  experimentTests.swift
+//  experimentTests
+//
+//  Created by Michael Lee on 2/7/26.
+//
+
+import Testing
+@testable import experiment
+
+struct experimentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
